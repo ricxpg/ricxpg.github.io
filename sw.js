280de26e17ba98b6a957ebee237d3627fa6bdfb4
ricks <80036401+ricxpg@users.noreply.github.com>
2025-10-24 10:37:55 +0200

    upload gpt stuff

---
+++ sw.js
@@ -0,0 +1,28 @@
+// bumpa la versione quando cambi i file per forzare l'update della cache
+const CACHE = 'checkin-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
